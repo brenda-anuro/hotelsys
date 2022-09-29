ee4d7e1f0aa3ad01c9e2dc510c3a08e0508c1016
brenda-anuro <suzannebrendah@gmail.com>
2022-09-29 20:01:09 +0300

    added login functionality

--- routes/admin.js
+++ routes/admin.js
@@ -1,15 +1,50 @@
 var express = require('express');
-const req = require('express/lib/request');
 var router = express.Router();
 
-/* GET users listing. */
+
+const adminCredentials = {
+  email: 'hotelsys@gmail.com',
+  password: 'brenda'
+}
+
+/* admin dashboard. */
 router.get('/', function(req, res, next) {
   res.render('admin/dashboard', {title: 'dashboard'});
 });
 
-// users login page
+// display admin login page
 router.get('/login', (req, res, next)=>{
-  res.render('admin/login', {title: 'login'});
+  const admin = {
+    email: '',
+    password: ''
+  }
+  res.render('admin/login', {title: 'login', admin:admin, error:false});
+})
+// submit admin login page
+router.post('/login',(req, res, next)=>{
+  const admin = {
+    email:req.body.email,
+    password: req.body.password
+  
+  }
+  if (adminCredentials.email === admin.email) {
+
+    if (adminCredentials.password === admin.password) {
+      req.session.user = 'admin'
+      res.redirect('/admin')
+      
+    } else {
+      let message = 'incorrect password'
+    res.render('admin/login', {title: 'login', admin:admin, error:true, message:message});
+
+    }
+    
+  } else {
+    let message = 'email does not exist'
+    res.render('admin/login', {title: 'login', admin:admin, error:true, message:message});
+    
+  }
+
 })
 
 
